a4d2b9669e91017db0310f370035e5c64a924576
Babosa1235468 <todribarbosa@gmail.com>
2025-11-04 19:36:44 +0000

    .

--- js/scriptPsico.js
+++ js/scriptPsico.js
@@ -1,16 +1,38 @@
-function MostrarPergunta(pergunta) {
-    let divsPerguntas = document.getElementsByClassName("pergunta");
-    for (i=0; i<divsPerguntas.length; i++){
-        if (divsPerguntas[i].id != "pergunta_" + pergunta){
-            divsPerguntas[i].style.display = "none";
-        }
+let x = 0;
+
+function MostrarPergunta() {
+    const perguntas = document.getElementsByClassName("pergunta");
+    const quadrados = document.querySelectorAll(".quadrado");
+    const imagem = document.getElementById("imagemCanto");
+
+    // Esconde todas perguntas
+    for (let i = 0; i < perguntas.length; i++) {
+        perguntas[i].style.display = "none";
     }
+
+    // Mostra próxima pergunta e ativa quadrado
+    if (x < perguntas.length) {
+        perguntas[x].style.display = "block";
+        quadrados[x].classList.add("ativo");
+
+        // aumenta a imagem proporcionalmente
+        const novoTamanho = 70 + (x+1) * 15; // cada quadrado aumenta 15px
+        imagem.style.width = novoTamanho + "px";
+
+        x++;
+    } 
 }
-function MostrarTodas(){
-    let divPergunta = document.getElementsByClassName("perguntas");
-    divPergunta.style.display = "";
-    let divsPerguntas = document.getElementsByClassName("pergunta");
-    for (i=0; i<divsPerguntas.length; i++){
-        divsPerguntas[i].style.display = "none";
+
+function EsconderTodas() {
+    const perguntas = document.getElementsByClassName("pergunta");
+    const quadrados = document.querySelectorAll(".quadrado");
+    const imagem = document.getElementById("imagemCanto");
+
+    for (let i = 0; i < perguntas.length; i++) {
+        perguntas[i].style.display = "none";
+        quadrados[i].classList.remove("ativo");
     }
+
+    imagem.style.width = "70px"; // reset imagem
+    x = 0;
 }
